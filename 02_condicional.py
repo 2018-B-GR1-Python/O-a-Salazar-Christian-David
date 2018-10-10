5924ca94d1408985881a20be67acf95b53c8495a
ChrisZoc <lobonop@gmail.com>
2018-10-10 08:54:56 -0500

    condicionales

---
+++ 02_condicional.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+if True:
+    print("Es verdaaaaad")
+else:
+    print("No es verdaaaaad")
+
+if False:
+    print("Es verdaaaaad")
+else:
+    print("No es verdaaaaad")
+
+nombre="Christian"
+
+if not nombre:
+    print("Si")
+else:
+    print("No")
